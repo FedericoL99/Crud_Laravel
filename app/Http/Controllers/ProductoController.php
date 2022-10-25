eaea2a7ea5ec9b98bc675ec96dbd87e95abfcbd7
FedericoL <floaiza@unal.edu.co>
2022-10-24 23:48:32 -0500

    V1

---
+++ app/Http/Controllers/ProductoController.php
@@ -0,0 +1,103 @@
+<?php
+
+namespace App\Http\Controllers;
+use App\Models\Producto;
+
+use Illuminate\Http\Request;
+use Productos;
+
+class ProductoController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        
+        $productos=Producto::all();
+        return view('index')->with('productos',$productos);
+        //return view('index');
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+        return view('gestionProductos');
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        $nombre=$request->input('nombre');
+        $description=$request->input('description');
+        $precio=$request->input('precio');
+
+        $producto=new Producto();
+        $producto->nombre=$nombre;
+        $producto->description=$description;
+        $producto->precio=$precio;
+        $producto->save();
+        return redirect()->route('productos.index'); 
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id)
+    {
+        //
+        $productos=Producto::find($id);
+        return view('actualizar',compact('productos'));
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        $producto=Producto::find($id);
+        $producto->delete();
+        return redirect()->route('productos.index');
+    }
+}
---
+++ resources/views/index.blade.php
@@ -0,0 +1,44 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Index</title>
+</head>
+<body>
+    <a href="{{route('productos.create')}}">Crear productos</a>
+
+    <table>
+        <thead>
+            <tr>
+                <th>Nombre</th>
+                <th>Descripcion</th>
+                <th>Precio</th>
+                <th>Opcion</th>
+            </tr>
+        </thead>
+        <tbody>
+            @foreach ($productos as $producto)
+                <tr>
+                    <td>{{$loop->iteration}}</td>
+                    <td>{{$producto->nombre}}</td>
+                    <td>{{$producto->description}}</td>
+                    <td>{{$producto->precio}}</td>
+                    <td>
+                        <a href="{{route('productos.edit',$producto->id)}}" method="post">
+                            @method('PUT')
+                            @csrf
+                            <button type="submit">Editar</button>
+                        </a>
+                        <form action="{{route('productos.destroy',$producto->id)}}" method="post">
+                            @method('DELETE')
+                            @csrf
+                            <button type="submit">Eliminar</button>
+                        </form>
+                    </td>
+                </tr>
+            @endforeach
+        </tbody>
+</body>
+</html>
