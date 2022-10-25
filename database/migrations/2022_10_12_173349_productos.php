31ababc04dd1bf49fa6205cecd39cb9d6f9bf06c
FedericoL <floaiza@unal.edu.co>
2022-10-24 23:54:52 -0500

    v1

---
+++ database/migrations/2022_10_12_173349_productos.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class Productos extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        //Para la creacion de la tabla productos
+        Schema::create('producto', function(Blueprint $table){
+            $table->id();
+            $table->string('nombre');
+            $table->string('description');
+            $table->string('precio');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        //
+    }
+}
---
+++ resources/views/actualizar.blade.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Actualizar</title>
+</head>
+<body>
+    <form action="{{route('productos.edit')}}" method="POST">
+        @csrf
+        <label>Nombre</label>
+        <input id="nombre" name="nombre" type="text" value="{{$productos->id}}"><br>
+        <label>Descripcion</label>
+        <input id="description" name="description" type="text" value="{{$productos->description}}"><br>
+        <label>Precio</label>
+        <input name="precio" type="precio" value="{{$productos->precio}}"><br>
+        <input type="submit" name="Enviar">
+    </form>
+</body>
+</html>
